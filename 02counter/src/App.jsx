b60ba3563fbb9c3eeddff5ba7f1c68dec5d6b4a9
MPrashant1100 <prashantmishra1100@gmail.com>
2023-12-06 22:26:30 +0530

    basic counter app created

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
+  const addCount = () => {
+    if ( count < 20 ) {
+      setCount(count + 1);
+    } else {
+       alert('Limit is 20 only')
+    }
+  };
+  const removeCount = () => {
+    if ( count > 0 ) {
+      setCount(count - 1);
+    } else {
+       alert('Limit is 0 only')
+    }
+  };
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Counter APP</h1>
+        <h1>Current Count: {count}</h1>
+        <button onClick={addCount}>Add count : {count}</button>
+        <button onClick={removeCount}>Remove count</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
