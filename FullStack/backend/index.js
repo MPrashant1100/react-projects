8ae5915ff45202bc63d48526873a5c167e4dfc20
MPrashant1100 <prashantmishra1100@gmail.com>
2024-04-20 22:13:10 +0530

    1. server created 2. connected to db

--- FullStack/backend/index.js
+++ FullStack/backend/index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import mongoose from 'mongoose';
 import { PORT, MONGODB_URI } from './config.js';
 
 const app = express();
@@ -7,8 +8,14 @@ app.get('/get', (req, res ) => {
    // console.log(req)
     return res.status(200).send("Welcome to server for backend")
 })
-app.listen(PORT, () => {
-    console.log(`Server is listen on:  ${PORT}`)
-})
 
-mongoose.connect( )
+mongoose.connect(MONGODB_URI)
+.then(() => {
+    console.log(" App connected to database ")
+    app.listen(PORT, () => {
+        console.log(`Server is listen on:  ${PORT}`)
+    })
+})
+.catch((error) => {
+    console.log(error)
+})
