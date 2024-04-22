75a954304e383c8b4406eb1a5cfd85c2b624b71e
MPrashant1100 <prashantmishra1100@gmail.com>
2024-04-22 11:41:54 +0530

    post api added

--- FullStack/backend/index.js
+++ FullStack/backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose';
 import { PORT, MONGODB_URI } from './config.js';
+import Book from './model/bookModel.js';
 
 const app = express();
 
@@ -9,7 +10,26 @@ app.get('/', (req, res ) => {
     return res.status(200).send("Welcome to server for backend")
 })
 
-app.post('/book', async ( req, res ) => {})
+app.post('/book', async ( req, res ) => {
+    try {
+        const { title, author, publishYear } = req.body;
+
+        if (!title || !author || !publishYear) {
+            return res.status(400),send({
+                message: 'Send all required fields: title, author, publishYear'
+            })
+        }
+        const newBook = {
+            title,
+            author,
+            publishYear,
+        }
+
+        const book = await Book.create(newBook)
+    } catch (error){
+        console.log( error)
+    }
+})
 
 mongoose.connect(MONGODB_URI)
 .then(() => {
