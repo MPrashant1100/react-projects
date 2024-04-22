d6e38ca96959615f27f075282637f11d3c9dd2c6
MPrashant1100 <prashantmishra1100@gmail.com>
2024-04-22 12:29:07 +0530

    get all api done

--- FullStack/backend/index.js
+++ FullStack/backend/index.js
@@ -5,6 +5,8 @@ import Book from './model/bookModel.js';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res ) => {
    // console.log(req)
     return res.status(200).send("Welcome to server for backend")
@@ -26,11 +28,30 @@ app.post('/book', async ( req, res ) => {
         }
 
         const book = await Book.create(newBook)
+
+        return res.status(201).send(book)
     } catch (error){
-        console.log( error)
+        console.log( error.message )
+        res.status(500).send({
+            message: error.message
+        })
     }
 })
 
+// Route for get all books from database 
+app.get('/book', async( req, res ) => {
+    try{
+       const books = await Book.find({})
+
+       return res.status(201).json(books)
+       
+    } catch (error){
+        console.log( error.message )
+        res.status(500).send({
+            message: error.message
+        })
+    }
+})
 mongoose.connect(MONGODB_URI)
 .then(() => {
     console.log(" App connected to database ")
